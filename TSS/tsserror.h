12356ffddd2c554c1d354da7fa8235b6052270a3
bitpick <bitpick@das-labor.org>
2014-04-02 12:09:44 +0200

    + adding function to return TSS errors as string (should save a lot of typing for those developing on TSS) ! fix documentation in dump.h

---
+++ TSS/tsserror.h
@@ -0,0 +1,9 @@
+
+#ifndef TSSERROR_H_
+#define TSSERROR_H_
+
+#include <stdint.h>
+
+char* TSS_getError_str( uint32_t err );
+
+#endif
--- dump.h
+++ dump.h
@@ -4,8 +4,9 @@
 /*
  * @param	addr	address of data to dump
  * @param	len	length of data to dump in bytes
- * @param	show_real_address	false	for relative address listing starting at 0x00
- * 					true	for real address listing starting at @addr
+ * @param	show_real_address	'false'	for relative address listing starting at 0x00
+ * 					'true'	for real address listing starting at @addr
+ * @param   show_ascii  'true' to show ASCII output
  */ 
 void dump_hex(void *addr, size_t len, bool show_real_address, bool show_ascii);
  
